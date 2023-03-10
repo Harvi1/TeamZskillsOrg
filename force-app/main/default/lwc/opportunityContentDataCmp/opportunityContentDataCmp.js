31f33a6cfa5c9201884ee4b52bb596e3437d2797
Harvi1 <thakkarharvi17@gmail.com>
2023-03-10 23:48:07 +0530

    added Icons and onclick open the document inNewTab

--- force-app/main/default/lwc/opportunityContentDataCmp/opportunityContentDataCmp.js
+++ force-app/main/default/lwc/opportunityContentDataCmp/opportunityContentDataCmp.js
@@ -5,13 +5,18 @@ import getoppwithContentdata from '@salesforce/apex/opportunityContentDataContro
 import updatecontentData from '@salesforce/apex/opportunityContentDataController.updateContentData';
 import retrieveContent from '@salesforce/apex/opportunityContentDataController.retrieveContent';
 import RevspaceImage from '@salesforce/resourceUrl/RevspaceImage';
-import Excel from '@salesforce/resourceUrl/Excel';
-import PDF from '@salesforce/resourceUrl/PDF';
+// import Excel from '@salesforce/resourceUrl/Excel';
+// import PDF from '@salesforce/resourceUrl/PDF';
+import forward from '@salesforce/resourceUrl/forward';
+import Liked from '@salesforce/resourceUrl/Liked';
+import Viewed from '@salesforce/resourceUrl/Viewed';
+import announcement from '@salesforce/resourceUrl/announcement';
+// import 	DOC from '@salesforce/resourceUrl/DOC';
+// import PPT from '@salesforce/resourceUrl/PPT';
 import STAGE_NAME from '@salesforce/schema/Opportunity.StageName';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+// import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const fields = [STAGE_NAME];
-
 export default class OpportunityContentDataCmp extends LightningElement {
     @track data = [];
     @track error;
@@ -25,15 +30,21 @@ export default class OpportunityContentDataCmp extends LightningElement {
     @api channelName = '/event/Content_Event__e';
     NoData = false;
     chevroniconShowHide = true;
-    visblityPlay = false;
-    visblityROI = false;
+    // visblityPlay = false;
+    // visblityROI = false;
     opportunityStageName;
-    visblityPlay;
+    //visblityPlay;
     @api recordId;
-
     RImage = RevspaceImage;
-    PDFIcon = PDF;
-    ExcelIcon = Excel;
+    //PDFIcon = PDF;
+    announcement = announcement;
+    forward = forward;
+    Liked = Liked;
+    Viewed = Viewed;
+    // ExcelIcon = Excel;
+    // DocIcon = DOC;
+    // PPTIcon = PPT;
+
 
     @wire(getRecord, { recordId: '$recordId', fields })
     opportunity;
@@ -45,23 +56,22 @@ export default class OpportunityContentDataCmp extends LightningElement {
     connectedCallback() {
         this.IsLoading = true;
 
-        // console.log('this.opportunityStageName--',this.recordId);
-
-        // this.opportunityStageName = getFieldValue(this.opportunity.data, STAGE_NAME)
-        this.records = [];
         getoppwithContentdata({ recordId: this.recordId })
             .then(result => {
                 var tempData = JSON.parse(JSON.stringify(result));
                 //this.records = tempData;
 
                 var conts = tempData;
-                for(var key in conts){
-                    this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
+                this.records = [];
+                for (var key in conts) {
+                    this.records.push({ value: conts[key], key: key }); //Here we are creating the array to show on UI.
                 }
 
-                
-                console.log('this.records---connected------'+JSON.stringify(this.records));
-                console.log('this.records---connected------'+this.records.length);
+                // if (this.records) {
+                //     this.records.forEach(item => item['document_url__c'] = '/lightning/r/Content__c/' + item['Id'] + '/view');
+                // }
+                //console.log('this.records---connected------' + JSON.stringify(this.records));
+                //console.log('this.records---connected------' + this.records.length);
                 //this.onsearchshowrecord = true;
                 if (this.records.length > 0) {
                     this.NoData = false;
@@ -70,6 +80,7 @@ export default class OpportunityContentDataCmp extends LightningElement {
                 }
                 this.IsLoading = false;
             }).catch(error => {
+                console.log('70 wire method called!! getoppwithContentdata', JSON.stringify(error));
                 this.records = [];
                 this.IsLoading = false;
                 this.NoData = true;
@@ -78,20 +89,20 @@ export default class OpportunityContentDataCmp extends LightningElement {
         this.registerErrorListener();
         this.handleSubscribe();
 
-        // console.log('this.opportunityStageName--',this.opportunity.data);
-        // console.log('this.opportunityStageName--',getFieldValue(this.opportunity.data, STAGE_NAME));
+        // //console.log('this.opportunityStageName--',this.opportunity.data);
+        // //console.log('this.opportunityStageName--',getFieldValue(this.opportunity.data, STAGE_NAME));
     }
 
     // @wire(getoppwithContentdata, { recordId: '$recordId' })
     // wiredContentData({ error, data }) {
     //     if (data) {
-    //         console.log('42 wire method called!! getoppwithContentdata', JSON.stringify(data));
+    //         //console.log('42 wire method called!! getoppwithContentdata', JSON.stringify(data));
     //         this.records = data;
     //         this.onsearchshowrecord = true;
     //         this.IsLoading = false;
     //     } else if (error) {
     //         this.error = error;
-    //         console.log('47 wire method called!! getoppwithContentdata', JSON.stringify(error));
+    //         //console.log('47 wire method called!! getoppwithContentdata', JSON.stringify(error));
     //         this.records = [];
     //         this.IsLoading = false;
     //         this.NoData = true;
@@ -101,27 +112,28 @@ export default class OpportunityContentDataCmp extends LightningElement {
     @wire(updatecontentData, { oppStage: '$oppstage' })
     updatedwiredContentData({ error, data }) {
         this.IsLoading = true;
-        this.records = [];
         if (data) {
             var tempdata = JSON.parse(JSON.stringify(data))
             //this.records = tempdata;
 
             var conts = tempdata;
+            this.records = [];
             for(var key in conts){
                 this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
             }
 
-            console.log('60 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
+            //console.log('60 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
             this.IsLoading = false;
             if (this.records.length > 0) {
-                console.log('63 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
+                //console.log('63 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
                 this.NoData = false;
             } else {
-                console.log('66 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
+                //console.log('66 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
                 this.NoData = true;
             }
         } else if (error) {
-            console.log('70 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
+            //console.log('70 wire method called!! getoppwithContentdata', JSON.stringify(this.records));
+            console.log('70 wire method called!! getoppwithContentdata', JSON.stringify(error));
             this.error = error;
             this.records = [];
             this.IsLoading = false;
@@ -130,7 +142,7 @@ export default class OpportunityContentDataCmp extends LightningElement {
     }
     handleSubscribe() {
         this.IsLoading = true;
-        console.log('handleSubscribe method called');
+        //console.log('handleSubscribe method called');
         const self = this;
         const messageCallback = function (response) {
             var obj = response;
@@ -141,12 +153,12 @@ export default class OpportunityContentDataCmp extends LightningElement {
         };
         // Invoke subscribe method of empApi. Pass reference to messageCallback
         subscribe(this.channelName, -1, messageCallback)
-            .then(response => {
-                // Response contains the subscription information on subscribe call
-                console.log('Subscription request sent to: ', JSON.stringify(response.channel));
-                this.IsLoading = false;
-                this.subscription = response;
-            });
+        .then(response => {
+            // Response contains the subscription information on subscribe call
+            //console.log('Subscription request sent to: ', JSON.stringify(response.channel));
+            this.IsLoading = false;
+            this.subscription = response;
+        });
     }
 
     registerErrorListener() {
@@ -162,140 +174,114 @@ export default class OpportunityContentDataCmp extends LightningElement {
     handleSearchchange(event) {
         this.searchStageName = event.target.value;
 
-        console.log('this.searchStageName lenght', this.searchStageName.length);
+        //console.log('this.searchStageName lenght', this.searchStageName.length);
         this.IsLoading = true;
         this.records = [];
 
         if (this.searchStageName === '') {
-            this.records = [];
             this.opportunityStageName = getFieldValue(this.opportunity.data, STAGE_NAME)
-            console.log('this.opportunityStageName--', this.opportunity.data);
 
-            retrieveContent({ keySearch: this.opportunityStageName })
-                .then(result => {
-                    var tempdata = JSON.parse(JSON.stringify(result));
-                    //this.records = tempData;
+            this.retriveContentData(this.opportunityStageName);
 
-                    var conts = tempdata;
-                    for(var key in conts){
-                        this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
-                    }
+            // retrieveContent({ keySearch: this.opportunityStageName })
+            // .then(result => {
 
-                    //this.onsearchshowrecord = true;
-                    if (this.records.length > 0) {
-                        this.NoData = false;
-                    } else {
-                        this.NoData = true;
-                    }
-                    this.IsLoading = false;
-                }).catch(error => {
-                    this.records = [];
-                    this.IsLoading = false;
-                    this.NoData = true;
-                })
-        } else {
-            // if (this.searchStageName.length <= 2) {
+            //     var tempdata = JSON.parse(JSON.stringify(result));
+
+            //     var conts = tempdata;
+            //     this.records = [];
+            //     for(var key in conts){
+            //         this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
+            //     }
+
+            //     //this.onsearchshowrecord = true;
+            //     if (this.records.length > 0) {
+            //         this.NoData = false;
+            //     } else {
+            //         this.NoData = true;
+            //     }
             //     this.IsLoading = false;
-            //     return
-            // }
+            // }).catch(error => {
+            //     console.log('retrieveContent---', JSON.stringify(error));
+            //     this.records = [];
+            //     this.IsLoading = false;
+            //     this.NoData = true;
+            // })
+        } else {
+
+            this.retriveContentData(this.searchStageName);
+            // retrieveContent({ keySearch: this.searchStageName })
+            // .then(result => {
+
+            //     var tempdata = JSON.parse(JSON.stringify(result));
+            //     var conts = tempdata;
+
+            //     this.records = [];
+            //     for(var key in conts){
+            //         this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
+            //     }
+
+            //     if (this.records.length > 0) {
+            //         this.NoData = false;
+            //     } else {
+            //         this.NoData = true;
+            //     }
+            //     this.IsLoading = false;
+                
+            // }).catch(error => {
+            //     console.log('retrieveContent---', JSON.stringify(error));
+            //     this.records = [];
+            //     this.IsLoading = false;
+            //     this.NoData = true;
+            // })
+        }
+    }
+
+    retriveContentData(stageName){
+        
+        retrieveContent({ keySearch: stageName })
+        .then(result => {
+
+            var tempdata = JSON.parse(JSON.stringify(result));
+            var conts = tempdata;
 
             this.records = [];
-            retrieveContent({ keySearch: this.searchStageName })
-                .then(result => {
-                    var tempdata = JSON.parse(JSON.stringify(result));
-                    //this.records = tempData;
+            for(var key in conts){
+                this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
+            }
 
-                    var conts = tempdata;
-                    for(var key in conts){
-                        this.records.push({value:conts[key], key:key}); //Here we are creating the array to show on UI.
-                    }
-
-                    //this.onsearchshowrecord = true;
-                    if (this.records.length > 0) {
-                        this.NoData = false;
-                    } else {
-                        this.NoData = true;
-                    }
-                    this.IsLoading = false;
-                }).catch(error => {
-                    this.records = [];
-                    this.IsLoading = false;
-                    this.NoData = true;
-                })
-        }
+            if (this.records.length > 0) {
+                this.NoData = false;
+            } else {
+                this.NoData = true;
+            }
+            this.IsLoading = false;
+            
+        }).catch(error => {
+            console.log('retrieveContent---', JSON.stringify(error));
+            this.records = [];
+            this.IsLoading = false;
+            this.NoData = true;
+        })
     }
 
     chevroniconShowHandleClick(event) {
         var recId = event.target.dataset.id;
-        
-        this.records.forEach(element => {
-            if (recId == element.Id) {
-                if (element.Show_Contents_Records__c === false) {
-                    element.Show_Contents_Records__c = true;
-                } else {
-                    element.Show_Contents_Records__c = false;
+
+        var seaRecords = JSON.parse(JSON.stringify(this.records));
+        seaRecords.forEach(element => {
+            element.value.forEach(val => {
+                if (recId == val.Id) {
+                    if (val.Show_Contents_Records__c === false) {
+                        val.Show_Contents_Records__c = true;
+                    } else {
+                        val.Show_Contents_Records__c = false;
+                    }
                 }
-            } 
+            });
         });
-        console.log(' this.records---------'+JSON.stringify(this.records));
-    }
-  /*  chevroniconHideHandleClick(event) {
-        /*const ct = event.target.dataset.id;
-        console.log('202 ct Id...', event.target.dataset.id);
-        visblityPlay = ct;
-        console.log('204 selectedContentId...',selectedContentId);
-        this.visblityPlay = false;
-        this.chevroniconShowHide = true;
-        console.log('Is visibility false???...',this.visblityPlay);
-    }*/
-    // handleEnter() {
-    //     this.IsLoading = true;
-    //     if (this.searchStageName !== '') {
-    //         retrieveContent({ keySearch: this.searchStageName })
-    //         .then(result => {
-    //             this.records = result;
-    //             this.onsearchshowrecord = true;
-    //             if (this.records.length > 0) {
-    //                 this.NoData = false;
-    //             } else {
-    //                 this.NoData = true;
-    //             }
-    //             this.IsLoading = false;
-    //         }).catch(error => {
-    //             this.records = [];
-    //             this.IsLoading = false;
-    //             this.NoData = true;
-    //         })
-    //     } 
-    //     else if(this.searchStageName == '') {
-    //         const event = new ShowToastEvent({
-    //             title: 'Information',
-    //             message: 'Please select the search value',
-    //             variant: 'info',
-    //             mode: 'dismissable'
-    //         });
-    //         this.dispatchEvent(event);
-    //         this.IsLoading = false;
-    //     }
-    // }
 
-    // get iconName1() {
-    //     if (this.visblityPlay) {
-    //         return 'utility:chevronup';
-            
-    //     } else {
-    //         return 'utility:chevrondown';
-    //     }
-    //     //return this.visblityPlay ? 'utility:chevronup' : 'utility:chevrondown';
-    // }
-
-    playReducehandleClick() {
-        this.visblityROI = false;
-        this.visblityPlay = !this.visblityPlay;
-    }
-
-    roiHandleClick() {
-        this.visblityPlay = false;
-        this.visblityROI = !this.visblityROI;
+        this.records = [];
+        this.records = JSON.parse(JSON.stringify(seaRecords));
     }
 }
